9a019fb5fd1c42e5cd52e59b0e992c7eff248d49
Hugo-Liang93 <hugo-lwh@hotmail.com>
2026-05-04 02:41:35 +0800

    feat: ExternalDataSource Protocol + source registry

---
+++ src/research/external/__init__.py
@@ -0,0 +1,22 @@
+"""External daily-resolution data sources for research feature providers.
+
+Public API: ExternalDataSource protocol + registry. Concrete sources
+(YFinanceClient, future FredClient/PolygonClient) live as siblings.
+"""
+from src.research.external.protocol import (
+    DailyBar,
+    ExternalDataSource,
+    UnknownSourceError,
+    get_source,
+    list_registered_sources,
+    register_source,
+)
+
+__all__ = [
+    "DailyBar",
+    "ExternalDataSource",
+    "UnknownSourceError",
+    "get_source",
+    "list_registered_sources",
+    "register_source",
+]
